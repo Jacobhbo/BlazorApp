b1bd4418cff24c94fc73fb63f62831158ef04fa3
jacob <jacob@JACOBHBO>
2023-03-21 01:41:04 +0100

    Add project files.

---
+++ BlazorApp1/Data/Chocolate.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorApp1.Data
+{
+    internal class Chocolate
+    {
+        public Chocolate()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+    }
+}
